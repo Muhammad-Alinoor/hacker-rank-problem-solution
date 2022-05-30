0edadd85921f9d374277e5c8a79d010159359a8a
Muhammad-Alinoor <alinoorm00@gmail.com>
2022-05-30 22:28:58 +0600

    if -else if

---
+++ weird-notweird.js
@@ -0,0 +1,15 @@
+const n = 3;
+if(n%2 == 0){
+    if(n >= 2 && n <= 5){
+        console.log('Not Weird');
+    }
+    else if(n >= 6 && n <= 20){
+        console.log('Weird');
+    }
+    else if(n > 20){
+        console.log('Not Weird');
+    }    
+}
+else if(n %2 != 0){
+        console.log('Weird');
+    }
